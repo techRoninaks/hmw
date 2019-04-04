5863dbb69366341a5d181da9eb03c590de851fd2
Srinath <srinathkamath@roninaks.com>
2019-04-04 23:27:53 +0530

    new changes to admin pages

---
+++ admin/assets/php/fetchTask.php
@@ -0,0 +1,20 @@
+<?php
+    require 'init.php';
+    header("Content-Type: application/json; charset=UTF-8");
+	
+	$id = $_POST["id"];
+    $sql = "SELECT * FROM `tasks` WHERE `ID`=$id";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result);
+    if($row){
+        $userId = $row["USER_ID"];
+        $sql = "SELECT * FROM users WHERE userId=$userId";
+        $result = mysqli_query($conn,$sql);
+        $row2 = mysqli_fetch_array($result);
+        $userData = array("taskId"=>$row["ID"],"userId"=>$row["USER_ID"],"userName"=>$row2["userName"],"targetPros"=>$row["PROSPECT_TARGET"],"targetLeads"=>$row["LEADS_TARGET"],"startDate"=>$row["DATE_START"],"endDate"=>$row["DATE_END"],"conversions"=>$row["CONVERSIONS"]);
+        $jsonData = json_encode($userData);
+        echo $jsonData;
+	} else {
+	    echo "0";
+	}
+?>
--- admin/assets/php/updateCategory.php
+++ admin/assets/php/updateCategory.php
@@ -8,11 +8,31 @@
     $result1 = mysqli_query($conn,$sql);
     $row = mysqli_fetch_array($result1);
     // echo "-1-".$sql;var_dump($row);
+    
+    if($data->image!=null || $data->image!= "" ){
+        $path = "../../../assets/img/cate_icons/";
+        $img = explode(",", $data->image);
+        define('UPLOAD_DIR', $path);
+        $img[1] = str_replace(' ', '+', $img[1]);
+        $imgData = base64_decode($img[1]);
+        $file = UPLOAD_DIR.$data->catName.'.png';
+        $success = file_put_contents($file, $imgData);
+    }
     if($row !== NULL){
+        if($success){
+            $address = "assets/img/cate_icons/"."$data->catName".".png";
+            $sql = "UPDATE `category` SET `name`='$data->catName',`tag`='$data->labelName',`union_name`='$data->unionName',image_address='$address' WHERE `name`='$data->cookieName'";
+        } else {
         $sql = "UPDATE `category` SET `name`='$data->catName',`tag`='$data->labelName',`union_name`='$data->unionName' WHERE `name`='$data->cookieName'";
+        }
     // echo "-2-".$sql;
     } else {
+        if($success){
+            $address = "assets/img/cate_icons/"."$data->catName".".png";
+            $sql = "INSERT INTO `category` (`name`,`tag`,`union_name`,`image_address`) VALUES ('$data->catName','$data->labelName','$data->unionName','$address')";
+        } else {
         $sql = "INSERT INTO `category` (`name`,`tag`,`union_name`) VALUES ('$data->catName','$data->labelName','$data->unionName')";
+        }
     // echo "-3-".$sql;
     }
     $result = mysqli_query($conn,$sql);
