d9c554f3eaad07444501464605594ea080cf2cb8
Roninaks Ventures Pvt. Ltd <40421354+techRoninaks@users.noreply.github.com>
2019-04-18 20:36:08 +0530

    Delete test.php

--- admin/assets/php/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require "init.php";
-    // $sql_query = "Select * from category;";
-    // $result = mysqli_query($con, $sql_query);
-    $t = "id,name,image_address,link,freq,color,tag,union_name,";
-    echo rtrim($t, ",");
-?>
