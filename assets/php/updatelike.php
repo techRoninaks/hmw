69590e0f75ca25428150ac41c8d7f51512bf67f7
ashishkamal17 <ashishak@roninaks.com>
2019-05-02 13:58:53 +0530

    Ashish: Likes on post(Profile and Service)

---
+++ assets/php/updatelike.php
@@ -0,0 +1,19 @@
+<?php
+require "init.php";//needed for connection with database
+    $action = $_POST["action"];
+    $userId = $_POST["userid"];
+    $postId = $_POST["postid"];
+    // echo $action."\n".$userId."\n".$postId."\n";
+    if($action == "add"){
+        // echo "like++"; 
+        $sql_query = "INSERT INTO `likes`(`userid`, `postid`) VALUES ( $userId , $postId )";
+        $result = mysqli_query($con,$sql_query);
+        // echo "sucess";
+    }
+     else if($action == "delete"){
+        // echo "like--"; 
+        $sql_query = "DELETE FROM `likes` WHERE userid = $userId AND postid = $postId";
+        $result = mysqli_query($con,$sql_query);
+        // echo $sql_query;
+    }
+?>
